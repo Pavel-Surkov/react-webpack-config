2bfbe818a67d29901fc15d79c1d30fcbc741fd30
Pavel Surkov <surkovv.pavel@gmail.com>
2022-06-20 17:38:44 +0700

    fixes issues

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -3,15 +3,15 @@ import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 
 const App: React.FC<string> = ({ text }) => {
-  return (
-    <React.StrictMode>
-      <div className="app">
-        <Header />
-        <div>{text}</div>
-        <Footer />
-      </div>
-    </React.StrictMode>
-  );
+	return (
+		<React.StrictMode>
+			<div className="app">
+				<Header />
+				<div>{text} + Dollar</div>
+				<Footer />
+			</div>
+		</React.StrictMode>
+	);
 };
 
 export default App;
