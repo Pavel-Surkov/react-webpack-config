65215385a06c10fe44668027708f365f21e36938
Pavel Surkov <surkovv.pavel@gmail.com>
2022-06-21 00:40:01 +0700

    adds farm output

--- src/js/components/AddHamster/AddHamster.jsx
+++ src/js/components/AddHamster/AddHamster.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from './AddHamster.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
-import { CHANGE_NAME, CHANGE_HEIGHT, CHANGE_WEIGHT } from '../../redux/actions/createActions';
+import {
+	CHANGE_NAME,
+	CHANGE_HEIGHT,
+	CHANGE_WEIGHT,
+	CLEAR
+} from '../../redux/actions/createActions';
 import { ADD_HAMSTER } from '../../redux/actions/farmActions';
+import axios from 'axios';
 
 const AddHamster = () => {
 	const dispatch = useDispatch();
 	const createHamster = useSelector((state) => state.create);
 	const farm = useSelector((state) => state.farm);
+
+	const [images, setImages] = useState(null);
+	const [count, setCount] = useState(0);
+
+	useEffect(() => {
+		axios
+			.get('https://random.dog/doggos')
+			.then((resp) => {
+				console.log(resp);
+				setImages(resp.data);
+			})
+			.catch((err) => console.log(err));
+	}, []);
 
 	const addHamster = () => {
 		const hamsterWithTheSameName = farm.find((hamster) => hamster.name === createHamster.name);
@@ -20,7 +39,8 @@ const AddHamster = () => {
 		const newHamster = {
 			name: createHamster.name,
 			weight: +createHamster.weight,
-			height: +createHamster.height
+			height: +createHamster.height,
+			image: `https://random.dog/${images[count]}`
 		};
 
 		if (newHamster.weight < 0) {
@@ -34,7 +54,10 @@ const AddHamster = () => {
 			return;
 		}
 
+		setCount((count) => count + 1);
+
 		dispatch(ADD_HAMSTER(newHamster));
+		dispatch(CLEAR());
 	};
 
 	return (
@@ -49,7 +72,7 @@ const AddHamster = () => {
 			</div>
 			<div className={classes.weight}>
 				<input
-					type="text"
+					type="number"
 					placeholder="Weight"
 					value={createHamster.weight}
 					onChange={(evt) => dispatch(CHANGE_WEIGHT(evt.target.value))}
@@ -57,7 +80,7 @@ const AddHamster = () => {
 			</div>
 			<div className={classes.height}>
 				<input
-					type="text"
+					type="number"
 					placeholder="Height"
 					value={createHamster.height}
 					onChange={(evt) => dispatch(CHANGE_HEIGHT(evt.target.value))}
--- src/js/redux/actions/createActions.js
+++ src/js/redux/actions/createActions.js
@@ -8,18 +8,25 @@ export const CHANGE_NAME = (name) => {
 export const CHANGE_WEIGHT = (weight) => {
 	return {
 		type: 'CHANGE_WEIGHT',
-		payload: weight
+		payload: +weight
 	};
 };
 
 export const CHANGE_HEIGHT = (height) => {
 	return {
 		type: 'CHANGE_HEIGHT',
-		payload: height
+		payload: +height
 	};
 };
 
-const CLEAR = () => {
+export const CHANGE_IMAGE = (img) => {
+	return {
+		type: 'CHANGE_IMAGE',
+		payload: img
+	};
+};
+
+export const CLEAR = () => {
 	return {
 		type: 'CLEAR'
 	};
