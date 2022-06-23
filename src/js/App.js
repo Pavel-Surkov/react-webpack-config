0af36bd48a397d12d65626b896e459fbbb639b1e
Pavel Surkov <surkovv.pavel@gmail.com>
2022-06-24 00:57:44 +0700

    configures webpack to work with react router

--- src/js/App.js
+++ src/js/App.js
@@ -3,15 +3,26 @@ import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { BrowserRouter, Route, Routes, Link, useParams } from 'react-router-dom';
+
+const Content = () => <div>Content</div>;
 
 const App = () => {
+	const path = useParams().id;
+	console.log(path);
+
 	return (
 		<Provider store={store}>
-			<div className="app">
-				<Header />
-				<div>Content</div>
-				<Footer />
-			</div>
+			<BrowserRouter>
+				<div className="app">
+					<Header />
+					<Link to="/content/doggos">ToContent</Link>
+					<Routes>
+						<Route path="/content/:id" element={<Content />} exact />
+					</Routes>
+					<Footer />
+				</div>
+			</BrowserRouter>
 		</Provider>
 	);
 };
